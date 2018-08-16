f2e88a582126a2c3b2b9488c389cc3b961684c33
sunghojang <sdjtei@gamil.com>
2018-08-16 16:31:18 +0900

    change directory

--- server/build-server-Desktop-Debug/moc_mainwindow.cpp
+++ server/build-server-Desktop-Debug/moc_mainwindow.cpp
@@ -1,13 +1,13 @@
 /****************************************************************************
 ** Meta object code from reading C++ file 'mainwindow.h'
 **
-** Created: Thu Aug 16 16:29:20 2018
+** Created: Thu Aug 16 16:28:43 2018
 **      by: The Qt Meta Object Compiler version 63 (Qt 4.8.2)
 **
 ** WARNING! All changes made in this file will be lost!
 *****************************************************************************/
 
-#include "../client/mainwindow.h"
+#include "../mainwindow.h"
 #if !defined(Q_MOC_OUTPUT_REVISION)
 #error "The header file 'mainwindow.h' doesn't include <QObject>."
 #elif Q_MOC_OUTPUT_REVISION != 63
@@ -17,13 +17,13 @@
 #endif
 
 QT_BEGIN_MOC_NAMESPACE
-static const uint qt_meta_data_Client[] = {
+static const uint qt_meta_data_Server[] = {
 
  // content:
        6,       // revision
        0,       // classname
        0,    0, // classinfo
-       5,   14, // methods
+       2,   14, // methods
        0,    0, // properties
        0,    0, // enums/sets
        0,    0, // constructors
@@ -32,71 +32,64 @@ static const uint qt_meta_data_Client[] = {
 
  // slots: signature, parameters, type, tag, flags
        8,    7,    7,    7, 0x08,
-      28,    7,    7,    7, 0x08,
-      54,   42,    7,    7, 0x08,
-      97,    7,    7,    7, 0x08,
-     122,    7,    7,    7, 0x08,
+      24,    7,    7,    7, 0x08,
 
        0        // eod
 };
 
-static const char qt_meta_stringdata_Client[] = {
-    "Client\0\0requestNewFortune()\0readFortune()\0"
-    "socketError\0displayError(QAbstractSocket::SocketError)\0"
-    "enableGetFortuneButton()\0sessionOpened()\0"
+static const char qt_meta_stringdata_Server[] = {
+    "Server\0\0sessionOpened()\0sendFortune()\0"
 };
 
-void Client::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, void **_a)
+void Server::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, void **_a)
 {
     if (_c == QMetaObject::InvokeMetaMethod) {
         Q_ASSERT(staticMetaObject.cast(_o));
-        Client *_t = static_cast<Client *>(_o);
+        Server *_t = static_cast<Server *>(_o);
         switch (_id) {
-        case 0: _t->requestNewFortune(); break;
-        case 1: _t->readFortune(); break;
-        case 2: _t->displayError((*reinterpret_cast< QAbstractSocket::SocketError(*)>(_a[1]))); break;
-        case 3: _t->enableGetFortuneButton(); break;
-        case 4: _t->sessionOpened(); break;
+        case 0: _t->sessionOpened(); break;
+        case 1: _t->sendFortune(); break;
         default: ;
         }
     }
+    Q_UNUSED(_a);
 }
 
-const QMetaObjectExtraData Client::staticMetaObjectExtraData = {
+const QMetaObjectExtraData Server::staticMetaObjectExtraData = {
     0,  qt_static_metacall 
 };
 
-const QMetaObject Client::staticMetaObject = {
-    { &QDialog::staticMetaObject, qt_meta_stringdata_Client,
-      qt_meta_data_Client, &staticMetaObjectExtraData }
+const QMetaObject Server::staticMetaObject = {
+    { &QDialog::staticMetaObject, qt_meta_stringdata_Server,
+      qt_meta_data_Server, &staticMetaObjectExtraData }
 };
 
 #ifdef Q_NO_DATA_RELOCATION
-const QMetaObject &Client::getStaticMetaObject() { return staticMetaObject; }
+const QMetaObject &Server::getStaticMetaObject() { return staticMetaObject; }
 #endif //Q_NO_DATA_RELOCATION
 
-const QMetaObject *Client::metaObject() const
+const QMetaObject *Server::metaObject() const
 {
     return QObject::d_ptr->metaObject ? QObject::d_ptr->metaObject : &staticMetaObject;
 }
 
-void *Client::qt_metacast(const char *_clname)
+void *Server::qt_metacast(const char *_clname)
 {
     if (!_clname) return 0;
-    if (!strcmp(_clname, qt_meta_stringdata_Client))
-        return static_cast<void*>(const_cast< Client*>(this));
+    if (!strcmp(_clname, qt_meta_stringdata_Server))
+        return static_cast<void*>(const_cast< Server*>(this));
     return QDialog::qt_metacast(_clname);
 }
 
-int Client::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
+int Server::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
 {
     _id = QDialog::qt_metacall(_c, _id, _a);
     if (_id < 0)
         return _id;
     if (_c == QMetaObject::InvokeMetaMethod) {
-        if (_id < 5)
+        if (_id < 2)
             qt_static_metacall(this, _c, _id, _a);
-        _id -= 5;
+        _id -= 2;
     }
     return _id;
 }
--- server/build-server-Desktop-Debug/ui_mainwindow.h
+++ server/build-server-Desktop-Debug/ui_mainwindow.h
@@ -1,7 +1,7 @@
 /********************************************************************************
 ** Form generated from reading UI file 'mainwindow.ui'
 **
-** Created: Thu Aug 16 16:29:18 2018
+** Created: Thu Aug 16 16:28:41 2018
 **      by: Qt User Interface Compiler version 4.8.2
 **
 ** WARNING! All changes made in this file will be lost when recompiling UI file!
